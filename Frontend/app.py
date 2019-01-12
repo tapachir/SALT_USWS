46da2bb0210f9a7de4c1ee50d7eee7902392abf1
tahir <tahir.oegel@pwc.com>
2019-01-12 16:59:39 +0100

    trying things

---
+++ Frontend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+def calc(latitude,longitude):
+    z = latitude + longitude
+    return z
+
+
+@app.route('/')
+def student():
+   return render_template('input.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      lat = request.form['Latitude']
+      lon = request.form['Longitude']
+      print(lat,lon)
+      finalResult = calc(lat,lon)
+      return render_template("result.html",result = finalResult)
+
+if __name__ == '__main__':  
+   app.run(debug = True)
