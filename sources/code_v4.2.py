59bd1b790a50da3c981a0032b1f457b6007b2a9b
FSMToph <benzmaximilian@web.de>
2019-01-17 19:31:58 +0100

    Bisschen verbessert
    TODO: Muss noch die ganzen Kategorien einfügen. Aktuell klappt das zählen in python nicht wie gewollt - bin dran

--- sources/code_v4.2.py
+++ sources/code_v4.2.py
@@ -8,8 +8,8 @@ import numpy as np
 from sklearn import linear_model
 import geopy.distance
 import sklearn
+import datetime
 
-# "all.csv" wird benötigt, da nur diese "categories" enthält, dafür aber die Koordinaten nur in einer Spalte (nicht zwei)
 df = pd.read_csv(r'C:\Users\max\Documents\Dev\Prog2\Abgabe4\SALT_USWS\sources\allWithCategory.csv')
 #renaming columns
 df.columns = ['latitude', 'longitude','rating','review_count','price','categories']
@@ -31,7 +31,7 @@ df['price'] = df['price'].fillna(2)
 
 df = df.dropna()
 
-df = df[0:300]
+df = df[0:100]
 
 #df splitten
 train_df, test_df = sklearn.model_selection.train_test_split(df, test_size= 0.2)
@@ -73,28 +73,32 @@ test_feature_df['own_category'] = 0
 train_feature_df1 = train_feature_df.drop(['categories'],axis=1)
 
 #Setzt den Radius für die Suche nach Nachbarn fest
-radius = 500
+radius = 750
 #Bereitet das train_dataframe fürs machinelearning for. füllt die features aus (anzahl der nachabrn)
+starttime = datetime.datetime.now()
+print('Startzeit:'+starttime)
 for x in range(len(train_feature_df)):
+    zeitx1 = datetime.datetime.now()
     if(len(train_feature_df)>=1000):
         modop_train = 5
     if(len(train_feature_df)<1000):
         modop_train = 10
     if((x /len(train_feature_df)*100) % modop_train ==0):
             print(str(x/len(train_feature_df)*100)+'%')
+            print('Aktuelle Dauer:'+ zeitx1-starttime)
     own_category = train_feature_df.at[x,'categories']
     coordinates1 = train_feature_df.at[x, 'latitude'], train_feature_df.at[x, 'longitude']
     for y in range(len(train_feature_df)):
         coordinates2 = train_feature_df.at[y, 'latitude'], train_feature_df.at[y, 'longitude']
         dist = geopy.distance.geodesic(coordinates1,coordinates2).m
         if(dist < radius and x != y):
-            if("Korean" in str(train_feature_df.at[y, 'categories'])):
+            if("korean" in str(train_feature_df.at[y, 'categories'])):
                 train_feature_df.at[x, 'koreanneigbours']+=1
-            if("Italian" in str(train_feature_df.at[y, 'categories'])):
+            if("italian" in str(train_feature_df.at[y, 'categories'])):
                 train_feature_df.at[x, 'italianneigbhours']+=1
-            if("Vietnamese" in str(train_feature_df.at[y, 'categories'])):
+            if("vietnamese" in str(train_feature_df.at[y, 'categories'])):
                 train_feature_df.at[x, 'vietnameseneigbhours']+=1
-            if("Japanese" in str(train_feature_df.at[y, 'categories'])):
+            if("japanese" in str(train_feature_df.at[y, 'categories'])):
                 train_feature_df.at[x, 'japaneseneigbhours']+=1
             if(own_category in str(train_feature_df.at[y, 'categories'])):
                 train_feature_df.at[x,'own_category']+=1
@@ -103,25 +107,27 @@ for x in range(len(train_feature_df)):
 
 #prepare test features
 for x in range(len(test_feature_df)):
+    zeitx2 = datetime.datetime.now()
     if(len(train_feature_df)>=500):
         modop_test = 5
     if(len(train_feature_df)<500):
         modop_test = 10
     if((x /len(test_feature_df)*100) % modop_test ==0):
             print(str(x/len(test_feature_df)*100)+'%')
+            print('Aktuelle Dauer:'+ zeitx2-starttime)
     own_category = test_feature_df.at[x,'categories']
     coordinates1 = test_feature_df.at[x, 'latitude'], test_feature_df.at[x, 'longitude']
     for y in range(len(test_feature_df)):
         coordinates2 = test_feature_df.at[y, 'latitude'], test_feature_df.at[y, 'longitude']
         dist = geopy.distance.geodesic(coordinates1,coordinates2).m
         if(dist < radius and x != y):
-            if("Korean" in str(test_feature_df.at[y, 'categories'])):
+            if("korean" in str(test_feature_df.at[y, 'categories'])):
                          test_feature_df.at[x, 'koreanneigbours']+=1
-            if("Italian" in str(test_feature_df.at[y, 'categories'])):
+            if("italian" in str(test_feature_df.at[y, 'categories'])):
                          test_feature_df.at[x, 'italianneigbhours']+=1
-            if("Vietnamese" in str(test_feature_df.at[y, 'categories'])):
+            if("vietnamese" in str(test_feature_df.at[y, 'categories'])):
                          test_feature_df.at[x, 'vietnameseneigbhours']+=1
-            if("Japanese" in str(test_feature_df.at[y, 'categories'])):
+            if("japanese" in str(test_feature_df.at[y, 'categories'])):
                          test_feature_df.at[x, 'japaneseneigbhours']+=1
             if(own_category in str(test_feature_df.at[y, 'categories'])):
                 test_feature_df.at[x,'own_category']+=1
@@ -129,9 +135,11 @@ for x in range(len(test_feature_df)):
                 test_feature_df.at[x, 'others']+=1
 
 ##exportiert das train_feature_df als .csv Datei in den Ordner, in dem diese Datei liegt
-#train_feature_df.to_csv('train_feature_df.csv', sep='\t', encoding='utf-8')
+train_feature_df.to_csv('train_feature_df.csv', sep='\t', encoding='utf-8')
 ##exportiert das test_feature_df als .csv Datei in den Ordner, in dem diese Datei liegt
-#test_feature_df.to_csv('test_feature_df.csv', sep='\t', encoding='utf-8')
+test_feature_df.to_csv('test_feature_df.csv', sep='\t', encoding='utf-8')
+##exportiert das test_df als .csv Datei in den Ordner, in dem diese Datei liegt
+test_df.to_csv('test_feature.csv', sep='\t', encoding='utf-8')
 
 #mainmethode
 def prediction(latitude,longitude,price,category):
@@ -150,13 +158,13 @@ def prediction(latitude,longitude,price,category):
         coordinates2 = near_df.at[y, 'latitude'], near_df.at[y, 'longitude']
         dist = geopy.distance.geodesic(coordinates1,coordinates2).m
         if(dist < radius):
-            if("Korean" in str(near_df.at[y, 'categories'])):
+            if("korean" in str(near_df.at[y, 'categories'])):
                          koreans+=1
-            if("Italian" in str(near_df.at[y, 'categories'])):
+            if("italian" in str(near_df.at[y, 'categories'])):
                          italians+=1
-            if("Vietnamese" in str(near_df.at[y, 'categories'])):
+            if("vietnamese" in str(near_df.at[y, 'categories'])):
                          vietnameses+=1
-            if("Japanese" in str(near_df.at[y, 'categories'])):
+            if("japanese" in str(near_df.at[y, 'categories'])):
                          japaneses+=1
             if(own_category in str(near_df.at[y, 'categories'])):
                          owncategory+=1
@@ -183,6 +191,7 @@ def gettingneighbours(latitude,longitude):
         
 #testing
 def test(test_df):
+    starttime2 = datetime.datetime.now()
     ergebnis_df = pd.DataFrame(columns=['score','predictedscore','dif'])
     ergebnis_df.at[0,'score'] = test_label_df.at[0,'score']
     if(len(test_df)>=500):
@@ -190,8 +199,10 @@ def test(test_df):
     if(len(test_df)<500):
         modop_test = 10
     for x in range(len(test_df)):
+        zeitx3 = datetime.datetime.now()
         if((x /len(test_df)*100) % modop_test ==0):
             print(str(x/len(test_df)*100)+'%')
+            print('Aktuelle Dauer:'+ zeitx3-starttime2)
         dif=0
         
         lat=test_df.at[x,'latitude']
@@ -206,8 +217,14 @@ def test(test_df):
         dif = abs(ergebnis_df.at[x,'score']-pred)
         ergebnis_df.at[x,'dif'] = dif
     print(ergebnis_df)
+    print('Score:')
+    print('Maximaler Score: ' + str(np.max(ergebnis_df['score'])))
+    print('Minimaler Score: ' + str(np.min(ergebnis_df['score'])))
+    print('Dif von Score:' + str(np.max(ergebnis_df['score'])-np.min(ergebnis_df['score'])))
+    print('PredictedScore:')
     print('Maximaler PredictedScore: ' + str(np.max(ergebnis_df['predictedscore'])))
     print('Minimaler PredictedScore: ' + str(np.min(ergebnis_df['predictedscore'])))
+    print('Dif:')
     print('Dif von PredictedScore:' + str(np.max(ergebnis_df['predictedscore'])-np.min(ergebnis_df['predictedscore'])))
     print('Mean:' + str(np.mean(ergebnis_df['dif'])))
     print('minimum: '+ str(np.min(ergebnis_df['dif'])))
